1c381639e3925ff5644483aea033fae4a34b0666
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-14 19:41:08 +0100

    Add QandA_Array containing the necessary questions and answers.

---
+++ src/QuestionsAndAnswers/qandA.js
@@ -0,0 +1,39 @@
+const QandA_Array = {
+  subject: "Mental health",
+  numOfQuestions: 3,
+  scorePerCorrectAnswer: 1,
+  questions: [
+    {
+      question: "Which of the following are real illnesses?",
+      answers: ["Diabetes", "Anxiety", "Flu", "All of the above"],
+      correct: "All of the above",
+    },
+    {
+      question: "Which of these symptoms can happen if you are depressed?",
+      answers: [
+        "Don't feel hungry",
+        "Constant fatigue",
+        "Lowered sex drive",
+        "All of the above",
+      ],
+      correct: "All of the above",
+    },
+    {
+      question: "How to tell if someone has an anxious disorder?",
+      answers: [
+        "Has bad breath",
+        "Untidy",
+        "Great speaker",
+        "Doesn't like to go out",
+      ],
+      correct: "Doesn't like to go out",
+    },
+    //   {
+    //     question: "",
+    //     answers: [],
+    //     correct: ''
+    //   },
+  ],
+};
+
+export { QandA_Array };
