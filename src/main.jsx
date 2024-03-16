1f64c40e53458ffbbc72db1f2463f27ebf1e497a
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-16 22:19:21 +0100

    Import global styles file.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// STYLES
+import "@styles/global.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
