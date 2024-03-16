b117a1d2d26b74fa941f2cda8f18ff6991b29ccf
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-16 22:20:36 +0100

    Make a Question component.

---
+++ src/components/Question.jsx
@@ -0,0 +1,12 @@
+const Question = ({ cur, all, question }) => {
+  return (
+    <section className="question">
+      <h1>
+        <span>{cur < 10 ? `0${cur}` : cur}</span>/{all < 10 ? `0${all}` : all}
+      </h1>
+      <p>{question}</p>
+    </section>
+  );
+};
+
+export default Question;
