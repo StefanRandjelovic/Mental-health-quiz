904ac1de4c0d173f7997f8016bb126df07e3214c
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-19 22:31:58 +0100

    Redo the NavButton component by separating functions into their own folders, and add additional functionalities.

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,9 +1,39 @@
-const NawButton = ({ promp, setCurrentQuestion }) => {
+// HELPER FUNCTIONS
+import { getText, handleNext } from "@helpers/helperFunctions.js";
+
+// STYLES
+import "@styles/NavButton.scss";
+
+const NavButton = ({
+  setCurrentQuestion,
+  userAnswer,
+  setUserAnswer,
+  setFinalResult,
+  questionsNum,
+  current,
+  setShowResults,
+  setHighlightenedAnswer,
+}) => {
   return (
-    <button onClick={() => setCurrentQuestion((prev) => prev + 1)}>
-      {promp()}
+    <button
+      className="navButton"
+      disabled={userAnswer === ""}
+      onClick={() =>
+        handleNext(
+          userAnswer,
+          setFinalResult,
+          current,
+          questionsNum,
+          setShowResults,
+          setUserAnswer,
+          setCurrentQuestion,
+          setHighlightenedAnswer
+        )
+      }
+    >
+      {getText(current, questionsNum)}
     </button>
   );
 };
 
-export default NawButton;
+export default NavButton;
