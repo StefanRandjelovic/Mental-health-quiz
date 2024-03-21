831c25123ce7d568b35a61a3dd1fde65ad235f4d
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-21 11:19:52 +0100

    Make available answers shuffle.

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,8 +1,9 @@
 // STYLES
 import "@styles/Answers.scss";
 
-// DEV DEPENDENCIES
-import { useState } from "react";
+// HELPER FUNCTIONS
+import { shuffle } from "@helpers/helperFunctions";
+import { useEffect, useState } from "react";
 
 const Answer = ({
   choices,
@@ -10,28 +11,43 @@ const Answer = ({
   correct,
   highlightenedAnswer,
   setHighlightenedAnswer,
+  currentQuestion,
 }) => {
+  const [shuffledChoices, setShuffledChoices] = useState(null);
+  const newChoices = structuredClone(choices);
+  useEffect(() => {
+    shuffle(newChoices);
+    setShuffledChoices(structuredClone(newChoices));
+  }, []);
+
+  useEffect(() => {
+    shuffle(newChoices);
+    setShuffledChoices(structuredClone(newChoices));
+  }, [currentQuestion]);
+  console.log(shuffledChoices);
+
   return (
     <section className="answer">
       <ul>
-        {choices.map((choice, index) => (
-          <div
-            key={crypto.randomUUID()}
-            className={highlightenedAnswer === index ? "selected" : null}
-          >
-            <li
-              onClick={(event) => {
-                if (event.target.innerHTML.slice(3) == correct) {
-                  setUserAnswer(true);
-                  setHighlightenedAnswer(index);
-                } else {
-                  setUserAnswer(false);
-                  setHighlightenedAnswer(index);
-                }
-              }}
-            >{`${index + 1}. ${choice}`}</li>
-          </div>
-        ))}
+        {shuffledChoices &&
+          shuffledChoices.map((choice, index) => (
+            <div
+              key={crypto.randomUUID()}
+              className={highlightenedAnswer === index ? "selected" : null}
+            >
+              <li
+                onClick={(event) => {
+                  if (event.target.innerHTML.slice(3) == correct) {
+                    setUserAnswer(true);
+                    setHighlightenedAnswer(index);
+                  } else {
+                    setUserAnswer(false);
+                    setHighlightenedAnswer(index);
+                  }
+                }}
+              >{`${index + 1}. ${choice}`}</li>
+            </div>
+          ))}
       </ul>
     </section>
   );
