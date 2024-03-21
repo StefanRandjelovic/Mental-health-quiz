c591a2efa7a82740251fa02719628f3e3a36639c
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-21 11:16:59 +0100

    Add new state variables, and redo props for new functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,14 @@ import { useState } from "react";
 import "@styles/App.scss";
 
 function App() {
+  // QUESTIONS AND ANSWERS
+  const { questions } = QandA_Array;
+
   // STATE VARIABLE
-  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [counter, setCounter] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    Math.floor(Math.random() * questions.length)
+  );
   const [userAnswer, setUserAnswer] = useState("");
   const [highlightenedAnswer, setHighlightenedAnswer] = useState(null);
   const [showResults, setShowResults] = useState(false);
@@ -25,32 +31,31 @@ function App() {
     wrongAnswers: 0,
   });
 
-  // QUESTIONS AND ANSWERS
-  const { questions } = QandA_Array;
-
   return (
     <main>
       {!showResults && (
         <>
           <Question
-            cur={currentQuestion}
-            all={questions.length}
-            question={questions[currentQuestion - 1].question}
+            cur={counter}
+            question={questions[currentQuestion].question}
           />
           <Answer
-            choices={questions[currentQuestion - 1].answers}
+            currentQuestion={currentQuestion}
+            choices={questions[currentQuestion].answers}
             setUserAnswer={setUserAnswer}
-            correct={questions[currentQuestion - 1].correct}
+            correct={questions[currentQuestion].correct}
             highlightenedAnswer={highlightenedAnswer}
             setHighlightenedAnswer={setHighlightenedAnswer}
           />
           <NavButton
+            setCounter={setCounter}
             setCurrentQuestion={setCurrentQuestion}
             userAnswer={userAnswer}
             setUserAnswer={setUserAnswer}
             setFinalResult={setFinalResult}
             questionsNum={questions.length}
             current={currentQuestion}
+            counter={counter}
             setShowResults={setShowResults}
             setHighlightenedAnswer={setHighlightenedAnswer}
           />
