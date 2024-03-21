5930115287fd59ae9c3e34931fd90e35c6b800c8
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-21 11:25:45 +0100

    Add new questions to an array.

--- src/QuestionsAndAnswers/qandA.js
+++ src/QuestionsAndAnswers/qandA.js
@@ -1,3 +1,4 @@
+// QUESTIONS AND ANSWERS ARRAY - App.jsx, helperFunctions.js
 const QandA_Array = {
   subject: "Mental health",
   numOfQuestions: 3,
@@ -5,8 +6,8 @@ const QandA_Array = {
   questions: [
     {
       question: "Which of the following are real illnesses?",
-      answers: ["Diabetes", "Anxiety", "Flu", "All of the above"],
-      correct: "All of the above",
+      answers: ["Diabetes", "Anxiety", "Flu", "All of the listed"],
+      correct: "All of the listed",
     },
     {
       question: "Which of these symptoms can happen if you are depressed?",
@@ -14,9 +15,9 @@ const QandA_Array = {
         "Don't feel hungry",
         "Constant fatigue",
         "Lowered sex drive",
-        "All of the above",
+        "All of the listed",
       ],
-      correct: "All of the above",
+      correct: "All of the listed",
     },
     {
       question: "How to tell if someone has an anxious disorder?",
@@ -27,6 +28,127 @@ const QandA_Array = {
         "Doesn't like to go out",
       ],
       correct: "Doesn't like to go out",
+    },
+    {
+      question: "What is assertiveness?",
+      answers: [
+        "A type of disease",
+        "A type of problematic behavior",
+        "Good communication style",
+        "None of the listed",
+      ],
+      correct: "Good communication style",
+    },
+    {
+      question: "Agitation is?",
+      answers: [
+        "A type of Greek yoghurt",
+        "A form of passive inexpedient behavior",
+        "A positive statement",
+        "Cummulative capabilities potential",
+      ],
+      correct: "A form of passive inexpedient behavior",
+    },
+    {
+      question: "What is demophobia?",
+      answers: [
+        "An unnatural fear of a crowd of people",
+        "A fear of democracy",
+        "Overwhelming fear of a certain demographic",
+        "None of the listed",
+      ],
+      correct: "An unnatural fear of a crowd of people",
+    },
+    {
+      question: "Depression is?",
+      answers: [
+        "A state of utter hardship",
+        "An uncommon mental health disorder",
+        "A state of listlessness, helplessness and hopelessness",
+        "None of the listed",
+      ],
+      correct: "A state of listlessness, helplessness and hopelessness",
+    },
+    {
+      question: "Who does depression hit harder?",
+      answers: [
+        "Young adults",
+        "Men in general",
+        "Women in general",
+        "Old people",
+      ],
+      correct: "Women in general",
+    },
+    {
+      question: "Destrudo is?",
+      answers: [
+        "A type of sword in Ancient Rome",
+        "Energy of the instinct of life",
+        "Energy of the instinct of death",
+        "Energy of the instinct of war",
+      ],
+      correct: "Energy of the instinct of death",
+    },
+    {
+      question: "What is a Diana complex?",
+      answers: [
+        "Unconscious repressed desire of a women to be a man",
+        "Unconscious repressed desire of a child to be an adult",
+        "Unconscious repressed desire of a man to be a woman",
+        "None of the listed",
+      ],
+      correct: "Unconscious repressed desire of a women to be a man",
+    },
+    {
+      question: "What is euphoria?",
+      answers: [
+        "A feeling of missing something",
+        "A feeling of dizziness",
+        "A feeling of lightness, pleasantness and exuberance",
+        "A feeling of loneliness",
+      ],
+      correct: "A feeling of lightness, pleasantness and exuberance",
+    },
+    {
+      question: "What is a phantasm?",
+      answers: [
+        "An objective real percieved reality",
+        "A subjective unrealistic representation of something in the waking state",
+        "A sense of dread",
+        "A sense of being decieved",
+      ],
+      correct:
+        "A subjective unrealistic representation of something in the waking state",
+    },
+    {
+      question: "What is hedonism?",
+      answers: [
+        "A love and preference of pleasure and enjoyment",
+        "A sense of joy from inflicting suffering",
+        "A state of permanently altered mindset",
+        "A preference towards good habits",
+      ],
+      correct: "A love and preference of pleasure and enjoyment",
+    },
+    {
+      question: "What is a dendrophobia?",
+      answers: [
+        "A fear of insects",
+        "A fear of dark places",
+        "A fear of trees",
+        "A fear of canines",
+      ],
+      correct: "A fear of trees",
+    },
+    {
+      question: "How shoud we proceed if we feel like we have mental problems?",
+      answers: [
+        "Stay home and rest",
+        "Seek a professionals help as soon as possible",
+        "Try to resolve the issues yourself",
+        "Deny that you have the problem",
+      ],
+      correct: "Seek a professionals help as soon as possible",
     },
     //   {
     //     question: "",
